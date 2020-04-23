d8ac04b7463520aa042a6adf4ba8935157e67e3f
Dennis Zelada <denniszelada@gmail.com>
2020-04-23 18:42:47 +0200

    Add the login form with antdesign

---
+++ src/user/Login.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Form, Input, Button, Checkbox } from 'antd';
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
+export default function Login() {
+  return (
+    <Form
+      {...layout}
+      name="basic"
+      initialValues={{
+        remember: true,
+      }}
+      onSubmit={e => e.preventDefault()}
+    >
+      <Form.Item
+        label="Username"
+        name="login-username"
+        id="login-username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="login-password"
+        id="login-password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  )
+}
