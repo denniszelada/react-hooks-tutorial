92727097ec9b0c9fbc32c12055985c02fb4228ac
Dennis Zelada <denniszelada@gmail.com>
2020-04-24 17:26:09 +0200

    Adding all the posts and userBar components on the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import PostList from './post/PostList';
 import CreatePost from './post/CreatePost';
+import UserBar from './user/UserBar';
 import './App.css';
 
+const user = 'Daniel B.'
+const posts = [
+  { title: 'React Hooks', content: 'The greatest thing since sliced bread', author: 'Daniel B.'},
+  { title: 'Using React Fragments', content: 'Keeping the DOM tree clean!', author: 'Daniel B.'},
+]
+
 export default function App() {
-  return <CreatePost />
+  return (
+    <div style={{ padding: 8 }}>
+      <UserBar />
+      <br />
+      <CreatePost user={user} />
+      <br />
+      <hr />
+      <PostList posts={posts} />
+    </div>
+  )
 }
