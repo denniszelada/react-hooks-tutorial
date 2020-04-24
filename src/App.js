bb8183cb51e7a645b0e6f8a4022b87b297b05fe4
Dennis Zelada <denniszelada@gmail.com>
2020-04-24 20:37:29 +0200

    Add user state to app and pass it to UserBar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PostList from './post/PostList';
 import CreatePost from './post/CreatePost';
 import UserBar from './user/UserBar';
 import './App.css';
 
-const user = 'Daniel B.'
 const posts = [
   { title: 'React Hooks', content: 'The greatest thing since sliced bread', author: 'Daniel B.'},
   { title: 'Using React Fragments', content: 'Keeping the DOM tree clean!', author: 'Daniel B.'},
 ]
 
 export default function App() {
+  const [ user, setUser ] = useState('')
   return (
     <div style={{ padding: 8 }}>
-      <UserBar />
+      <UserBar user={user} setUser={setUser} />
       <br />
-      <CreatePost user={user} />
+    {user && <CreatePost user={user} />}
       <br />
       <hr />
       <PostList posts={posts} />
