e163adf72a174d7007af379e4e3acf5208cb3b2e
Dennis Zelada <denniszelada@gmail.com>
2020-04-25 11:45:10 +0200

    Adjust the create post component to use the useState hook

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import CreatePost from './post/CreatePost';
 import UserBar from './user/UserBar';
 import './App.css';
 
-const posts = [
+const defaultPosts = [
   { title: 'React Hooks', content: 'The greatest thing since sliced bread', author: 'Daniel B.'},
   { title: 'Using React Fragments', content: 'Keeping the DOM tree clean!', author: 'Daniel B.'},
 ]
 
 export default function App() {
   const [ user, setUser ] = useState('')
+  const [ posts, setPosts ] = useState(defaultPosts)
   return (
     <div style={{ padding: 8 }}>
       <UserBar user={user} setUser={setUser} />
       <br />
-    {user && <CreatePost user={user} />}
+    {user && <CreatePost user={user} posts={posts} setPosts={setPosts} />}
       <br />
       <hr />
       <PostList posts={posts} />
