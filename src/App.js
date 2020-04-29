2942884b460e794f1212bafde519670da8bf5abb
Dennis Zelada <denniszelada@gmail.com>
2020-04-29 19:47:47 +0200

    Add useReducer to the posts

--- src/App.js
+++ src/App.js
@@ -21,14 +21,24 @@ function userReducer (state, action) {
   }
 }
 
+function postsReducer (state, action) {
+  switch (action.type) {
+    case 'CREATE_POST':
+      const newPost = { title: action.title, content: action.content, author: action.author }
+      return [ newPost, ...state ]
+    default:
+      throw new Error()
+  }
+}
+
 export default function App() {
   const [ user, dispatchUser ] = useReducer(userReducer, '')
-  const [ posts, setPosts ] = useState(defaultPosts)
+  const [ posts, dispatchPosts ] = useReducer(postsReducer, defaultPosts)
   return (
     <div style={{ padding: 8 }}>
       <UserBar user={user} dispatch={dispatchUser} />
       <br />
-    {user && <CreatePost user={user} posts={posts} setPosts={setPosts} />}
+    {user && <CreatePost user={user} posts={posts} dispatch={dispatchPosts} />}
       <br />
       <hr />
       <PostList posts={posts} />
