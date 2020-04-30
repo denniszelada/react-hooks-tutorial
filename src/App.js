871efd9f5377d7a267aa32e5aea6dc430b999aad
Dennis Zelada <denniszelada@gmail.com>
2020-04-30 16:16:32 +0200

    Add centralized appReducer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useReducer } from 'react';
 import PostList from './post/PostList';
 import CreatePost from './post/CreatePost';
 import UserBar from './user/UserBar';
+import appReducer from './reducers'
 import './App.css';
 
 const defaultPosts = [
@@ -9,36 +10,15 @@ const defaultPosts = [
   { title: 'Using React Fragments', content: 'Keeping the DOM tree clean!', author: 'Daniel B.'},
 ]
 
-function userReducer (state, action) {
-  switch (action.type) {
-    case 'LOGIN':
-    case 'REGISTER':
-      return action.username
-    case 'LOGOUT':
-      return ''
-    default:
-      throw new Error()
-  }
-}
-
-function postsReducer (state, action) {
-  switch (action.type) {
-    case 'CREATE_POST':
-      const newPost = { title: action.title, content: action.content, author: action.author }
-      return [ newPost, ...state ]
-    default:
-      throw new Error()
-  }
-}
 
 export default function App() {
-  const [ user, dispatchUser ] = useReducer(userReducer, '')
-  const [ posts, dispatchPosts ] = useReducer(postsReducer, defaultPosts)
+  const [ state, dispatch ] = useReducer(appReducer, { user: '', posts: defaultPosts })
+  const { user, posts } = state
   return (
     <div style={{ padding: 8 }}>
-      <UserBar user={user} dispatch={dispatchUser} />
+      <UserBar user={user} dispatch={dispatch} />
       <br />
-    {user && <CreatePost user={user} posts={posts} dispatch={dispatchPosts} />}
+    {user && <CreatePost user={user} posts={posts} dispatch={dispatch} />}
       <br />
       <hr />
       <PostList posts={posts} />
