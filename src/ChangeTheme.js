b120ab68bf9aef723eeb8150a0b080a27c19cfc9
Dennis Zelada <denniszelada@gmail.com>
2020-05-01 16:00:19 +0200

    Fetch the themes from the server with useEffect and useState

--- src/ChangeTheme.js
+++ src/ChangeTheme.js
@@ -1,9 +1,4 @@
-import React from 'react'
-
-const THEMES= [
-  { primaryColor: 'deepskyblue', secondaryColor: 'coral' },
-  { primaryColor: 'orchid', secondaryColor: 'mediumseagreen'}
-]
+import React, { useEffect, useState } from 'react'
 
 function ThemeItem ({ theme, active, onClick }) {
   return (
@@ -15,6 +10,14 @@ function ThemeItem ({ theme, active, onClick }) {
 }
 
 export default function ChangeTheme ({ theme, setTheme }) {
+  const [ themes, setThemes ] = useState([])
+
+  useEffect(() => {
+    fetch('/api/themes')
+    .then(result => result.json())
+    .then(themes => setThemes(themes))
+  }, [])
+
   function isActive(t) {
     return t.primaryColor === theme.primaryColor && t.secondaryColor === theme.secondaryColor
   }
@@ -22,7 +25,7 @@ export default function ChangeTheme ({ theme, setTheme }) {
   return (
     <div>
       Change theme:
-      {THEMES.map((t, i) =>
+      {themes.map((t, i) =>
         <ThemeItem key={'theme-' + i} theme={t} active={isActive(t)} onClick={() => setTheme(t)} />
       )}
     </div>
