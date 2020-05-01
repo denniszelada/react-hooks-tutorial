f1f72085b3e6d26289dc5576ad1cc2ad9cb8943b
Dennis Zelada <denniszelada@gmail.com>
2020-05-01 15:51:45 +0200

    Add dummy server and proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,13 @@
-const proxy = require('http-proxy-middleware')
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(proxy('/api', {
-    target: 'http://localhost:4000',
-    pathRewrite: { '^/api':'' }
-  }))
-}
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:4000',
+      changeOrigin: true,
+      pathRewrite: { '^/api': '' }
+    })
+  );
+};
+
